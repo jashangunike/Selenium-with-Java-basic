431fb9b7231122ea649b3f1443a01a8084e6fc7b
jashan-PC\jashan <jashangunike@gmail.com>
2018-04-22 00:46:28 +0530

    if Else Program updated and readme updated

--- SeleniumJava/src/JavaBasic/ifelse.java
+++ SeleniumJava/src/JavaBasic/ifelse.java
@@ -8,9 +8,45 @@ package JavaBasic;
 public class ifelse {
     public static void main(String[] args) {
 
+      // < >  >= <= == !=
 
+      int a = 10;
+      int b = 20;
 
+      if(b>a)
+      {
+          System.out.println("B is greater than a" );
+      }
+      else {
+          System.out.println("A is greater than B");
 
+      }
+
+    int c = 10;
+
+      if (c==10) {
+          System.out.println("Pass");
+      }
+      else {
+          System.out.println("Fail");
+      }
+
+      int p = 5;
+      int q = 10;
+      int k = p + q;
+
+      if (k>20)
+      {
+          System.out.println("fail");
+      }
+      else {
+          System.out.println("pass");
+      }
+
+      if(true)
+      {
+          System.out.println("Pass the True Condition");
+      }
 
 
     }
